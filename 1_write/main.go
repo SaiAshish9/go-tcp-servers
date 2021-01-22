c09c106a482574d8f098cf280881738ef09f185d
saiashish9 <saiashish3760@gmail.com>
2021-01-22 23:27:53 +0530

    telnet command updated

--- 1_write/main.go
+++ 1_write/main.go
@@ -7,12 +7,16 @@ import (
 	"net"
 )
 
+// IETF
+
 func main() {
-	li, err := net.Listen("tcp", ":8080")
+	li, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer li.Close()
+
+	// telnet localhost 8081
 
 	for {
 		conn, err := li.Accept()
@@ -20,11 +24,9 @@ func main() {
 			log.Println(err)
 			continue
 		}
-
 		io.WriteString(conn, "\nHello from TCP server\n")
 		fmt.Fprintln(conn, "How is your day?")
 		fmt.Fprintf(conn, "%v", "Well, I hope!")
-
 		conn.Close()
 	}
 }
